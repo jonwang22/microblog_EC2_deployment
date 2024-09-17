5acd12259afd0772e5c863519d8f44f70d3a7f2c
Ubuntu <ubuntu@ip-172-31-30-144.ec2.internal>
2024-09-17 20:06:18 +0000

    adding login page test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,1 +1,14 @@
-print("it works")
+# Unit Testing
+import pytest
+from microblog import app
+
+# Testing website HTTP Get requests
+@pytest.fixture
+def test_website():
+    app.config.update({
+        "TESTING": True,
+    })
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
